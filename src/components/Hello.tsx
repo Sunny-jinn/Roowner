791b1b73fe5eaaa5430d009666f75a66bd8ad196
Sunny-jinn <rlawlsdn316@gmail.com>
2024-02-04 23:13:11 +0900

    feat: add grid for map

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,17 +1,18 @@
 "use client";
 
-import { OrbitControls } from "@react-three/drei";
+import { Grid, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 
 const Hello = () => {
   return (
-    <Canvas>
-      <mesh>
-        <planeGeometry args={[1, 1]} />
+    <Canvas camera={{ position: [8, 8, 8], fov: 30 }}>
+      <mesh rotation-x={Math.PI / 2} position={[1, 1, 1]}>
+        <planeGeometry args={[5, 5]} />
         <meshStandardMaterial color={"#f0f0f0"} />
       </mesh>
       <directionalLight position={[1, 2, 3]} />
       <OrbitControls />
+      <Grid infiniteGrid fadeDistance={50} fadeStrength={5} />
     </Canvas>
   );
 };
